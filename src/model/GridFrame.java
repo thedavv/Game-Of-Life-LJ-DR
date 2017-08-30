68b0ff2effc19b504a90bb821b44f4a36bbebf2b
thedavv <thedavv@users.noreply.github.com>
2017-08-30 22:41:02 +0200

    changed modificators, implemented clear

--- src/model/GridFrame.java
+++ src/model/GridFrame.java
@@ -14,26 +14,27 @@ public class GridFrame extends JFrame {
 	private static final List<String> menuItemsNames = Arrays.asList("Step", "Start", "Stop", "Clear",
 			"Exit Application");
 
-	JMenuBar menuBar = new JMenuBar();
-	JMenu menu = new JMenu("Game Actions");
-	JMenuItem step = new JMenuItem(menuItemsNames.get(0));
-	JMenuItem start = new JMenuItem(menuItemsNames.get(1));
-	JMenuItem stop = new JMenuItem(menuItemsNames.get(2));
-	JMenuItem clear = new JMenuItem(menuItemsNames.get(3));
-	JMenuItem exit = new JMenuItem(menuItemsNames.get(4));
+	private GridComponent gridC = new GridComponent();
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu menu = new JMenu("Game Actions");
+	private JMenuItem step = new JMenuItem(menuItemsNames.get(0));
+	private JMenuItem start = new JMenuItem(menuItemsNames.get(1));
+	private JMenuItem stop = new JMenuItem(menuItemsNames.get(2));
+	private JMenuItem clear = new JMenuItem(menuItemsNames.get(3));
+	private JMenuItem exit = new JMenuItem(menuItemsNames.get(4));
 
 	public GridFrame() {
-		GridComponent gridC = new GridComponent();
+		
 
 		// for test purposes
-		gridC.sqGrid.get(2).get(5).setActivity(true);
-		gridC.sqGrid.get(0).get(0).setActivity(true);
-		gridC.sqGrid.get(8).get(1).setActivity(true);
-		System.out.println(gridC.sqGrid.get(5).get(5).toString());
-		System.out.println(gridC.sqGrid.get(0).get(0).toString());
-		System.out.println(gridC.sqGrid.get(8).get(1).toString());
-		System.out.println(gridC.sqGrid.get(4).get(9).toString());
-		System.out.println(gridC.sqGrid.get(3).get(7).toString());
+		gridC.getSqGrid().get(2).get(5).setActivity(true);
+		gridC.getSqGrid().get(0).get(0).setActivity(true);
+		gridC.getSqGrid().get(8).get(1).setActivity(true);
+		System.out.println(gridC.getSqGrid().get(5).get(5).toString());
+		System.out.println(gridC.getSqGrid().get(0).get(0).toString());
+		System.out.println(gridC.getSqGrid().get(8).get(1).toString());
+		System.out.println(gridC.getSqGrid().get(4).get(9).toString());
+		System.out.println(gridC.getSqGrid().get(3).get(7).toString());
 
 		// adding components to frame
 		createMenuForGridFrame();
@@ -73,8 +74,10 @@ public class GridFrame extends JFrame {
 		exit.addActionListener((ActionEvent e) -> {
 			dispose();
 		});
+		
+		//TODO dokoncit pre druhe pole
 		clear.addActionListener((ActionEvent e) -> {
-			// TODO reset fields
+			gridC.resetGameGrid(gridC.getSqGrid());
 		});
 	}
 }
