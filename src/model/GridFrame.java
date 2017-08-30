f7bcca9220aa1a70d4f707d4f2a0dd5d945cab79
thedavv <thedavv@users.noreply.github.com>
2017-08-30 21:59:16 +0200

    added menu for app

--- src/model/GridFrame.java
+++ src/model/GridFrame.java
@@ -1,32 +1,80 @@
 package model;
 
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class GridFrame extends JFrame {
-
 	private static final long serialVersionUID = 1L;
+	private static final List<String> menuItemsNames = Arrays.asList("Step", "Start", "Stop", "Clear",
+			"Exit Application");
+
+	JMenuBar menuBar = new JMenuBar();
+	JMenu menu = new JMenu("Game Actions");
+	JMenuItem step = new JMenuItem(menuItemsNames.get(0));
+	JMenuItem start = new JMenuItem(menuItemsNames.get(1));
+	JMenuItem stop = new JMenuItem(menuItemsNames.get(2));
+	JMenuItem exit = new JMenuItem(menuItemsNames.get(3));
+	JMenuItem clear = new JMenuItem(menuItemsNames.get(4));
 
 	public GridFrame() {
-		// all the print functions are for debugging
 		GridComponent gridC = new GridComponent();
+
+		// for test purposes
 		gridC.sqGrid.get(2).get(5).setActivity(true);
 		gridC.sqGrid.get(0).get(0).setActivity(true);
 		gridC.sqGrid.get(8).get(1).setActivity(true);
 		System.out.println(gridC.sqGrid.get(5).get(5).toString());
-		System.out.println(gridC.numOfActiveNeighbors(gridC.sqGrid.get(5).get(5)));
 		System.out.println(gridC.sqGrid.get(0).get(0).toString());
-		System.out.println(gridC.numOfActiveNeighbors(gridC.sqGrid.get(1).get(0)));
 		System.out.println(gridC.sqGrid.get(8).get(1).toString());
-		System.out.println(gridC.numOfActiveNeighbors(gridC.sqGrid.get(8).get(0)));
 		System.out.println(gridC.sqGrid.get(4).get(9).toString());
-		System.out.println(gridC.numOfActiveNeighbors(gridC.sqGrid.get(4).get(9)));
 		System.out.println(gridC.sqGrid.get(3).get(7).toString());
-		System.out.println(gridC.numOfActiveNeighbors(gridC.sqGrid.get(3).get(7)));
+
+		// adding components to frame
+		createMenuForGridFrame();
+		setJMenuBar(menuBar);
 		add(gridC);
 		pack();
 		setLocationRelativeTo(null);
 
+		// test grid
 		gridC.printGrid();
 
+		// adding action listeners
+		addMenuEventsToGridFrame();
+	}
+
+	private void createMenuForGridFrame() {
+		menuBar.add(menu);
+		menu.add(step);
+		menu.addSeparator();
+		menu.add(start);
+		menu.add(stop);
+		menu.add(clear);
+		menu.addSeparator();
+		menu.add(exit);
+	}
+
+	private void addMenuEventsToGridFrame() {
+		step.addActionListener((ActionEvent e) -> {
+			// TODO dokoncit
+		});
+		start.addActionListener((ActionEvent e) -> {
+			// TODO dokoncit
+		});
+		stop.addActionListener((ActionEvent e) -> {
+			// TODO dokoncit
+		});
+		exit.addActionListener((ActionEvent e) -> {
+			dispose();
+		});
+		clear.addActionListener((ActionEvent e) -> {
+			// TODO reset fields
+		});
 	}
 }
