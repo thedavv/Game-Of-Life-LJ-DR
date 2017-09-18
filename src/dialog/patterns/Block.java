543ebd1f0ca8df681bc9d2c76a6e083fd31be54f
thedavv <thedavv@users.noreply.github.com>
2017-09-19 00:40:01 +0200

    Created Class for Block pattern

--- src/dialog/patterns/Block.java
+++ src/dialog/patterns/Block.java
@@ -1,12 +1,18 @@
 package dialog.patterns;
 
-import model.GridComponent;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Block implements Patterns{
+public class Block implements Patterns {
 
 	@Override
-	public GridComponent createPattern(GridComponent grid, String patternType) {
-		return null;
-	}
+	public List<Integer[]> createPattern() {
+		List<Integer[]> storedPatternPositons = new ArrayList<>();
+		storedPatternPositons.add(new Integer[] { 0, 0 });
+		storedPatternPositons.add(new Integer[] { 0, 1 });
+		storedPatternPositons.add(new Integer[] { 1, 0 });
+		storedPatternPositons.add(new Integer[] { 1, 1 });
 
+		return storedPatternPositons;
+	}
 }
