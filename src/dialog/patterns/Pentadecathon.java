dfb9c7eb7f8e86a664d08be71974d8eb8c7f792a
thedavv <thedavv@users.noreply.github.com>
2017-09-19 19:06:11 +0200

    added clases for gliders and oscilators

---
+++ src/dialog/patterns/Pentadecathon.java
@@ -0,0 +1,40 @@
+package dialog.patterns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class Pentadecathon implements Pattern, GifPattern{
+
+	@Override
+	public List<Integer[]> createPattern() {
+		List<Integer[]> storedPatternPositons = new ArrayList<>();
+		storedPatternPositons.add(new Integer[] { 0, 1 });
+		storedPatternPositons.add(new Integer[] { 1, 1 });
+		
+		storedPatternPositons.add(new Integer[] { 2, 0 });
+		storedPatternPositons.add(new Integer[] { 2, 2 });
+		
+		storedPatternPositons.add(new Integer[] { 3, 1 });
+		storedPatternPositons.add(new Integer[] { 4, 1 });
+		storedPatternPositons.add(new Integer[] { 5, 1 });
+		storedPatternPositons.add(new Integer[] { 6, 1 });
+		
+		storedPatternPositons.add(new Integer[] { 7, 0 });
+		storedPatternPositons.add(new Integer[] { 7, 2 });
+		
+		storedPatternPositons.add(new Integer[] { 8, 1 });
+		storedPatternPositons.add(new Integer[] { 9, 1 });
+		
+		return storedPatternPositons;
+	}
+
+	@Override
+	public Icon createGif(String path) {
+		 Icon icon = new ImageIcon(path);
+		return icon;
+	}
+
+}
