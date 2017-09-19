3168585de1f8075d18e47bc73c06411dcce966de
thedavv <thedavv@users.noreply.github.com>
2017-09-19 19:05:31 +0200

    added creation for ImgPattern and GifPattern interface and its patterns

--- src/dialog/patterns/PatternFactory.java
+++ src/dialog/patterns/PatternFactory.java
@@ -2,7 +2,7 @@ package dialog.patterns;
 
 public class PatternFactory {
 
-	public Pattern createPattern(String patternName) {
+	public ImagePattern createImagePattern(String patternName) {
 		switch (patternName) {
 		case "Block":
 			return new Block();
@@ -14,6 +14,29 @@ public class PatternFactory {
 			return new Boat();
 		case "Tub":
 			return new Tub();
+		case "Gosper glider gun":
+			return new GosperGliderGun();
+		default:
+			return null;
+		}
+	}
+
+	public GifPattern createGifPattern(String patternName) {
+		switch (patternName) {
+		case "Blinker":
+			return new Blinker();
+		case "Toad":
+			return new Toad();
+		case "Beacon":
+			return new Beacon();
+		case "Pulsar":
+			return new Pulsar();
+		case "Pentadecathon":
+			return new Pentadecathon();
+		case "Glider":
+			return new Glider();
+		case "LWSS":
+			return new LWSS();
 		default:
 			return null;
 		}
