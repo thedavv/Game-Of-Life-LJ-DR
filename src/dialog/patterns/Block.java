9cdc6a4355afbf47b9a6bf1bb5d065c49a492dab
thedavv <thedavv@users.noreply.github.com>
2017-09-19 10:21:55 +0200

    lock pattern class

--- src/dialog/patterns/Block.java
+++ src/dialog/patterns/Block.java
@@ -1,7 +1,12 @@
 package dialog.patterns;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 public class Block implements Patterns {
 
@@ -14,5 +19,11 @@ public class Block implements Patterns {
 		storedPatternPositons.add(new Integer[] { 1, 1 });
 
 		return storedPatternPositons;
+	}
+
+	@Override
+	public BufferedImage createImage(String path) throws IOException {
+		BufferedImage image = ImageIO.read(new File(path));
+		return image;
 	}
 }
