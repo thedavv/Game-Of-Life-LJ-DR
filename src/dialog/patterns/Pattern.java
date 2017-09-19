c4d93eeeba261adba3ffb31b7c2b29dbc8c52471
thedavv <thedavv@users.noreply.github.com>
2017-09-19 19:06:27 +0200

    rewritten pattern Interface to hold only create pattern

--- src/dialog/patterns/Pattern.java
+++ src/dialog/patterns/Pattern.java
@@ -1,7 +1,5 @@
 package dialog.patterns;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -16,15 +14,4 @@ public interface Pattern {
 	 *         coordinations of active fields
 	 */
 	public List<Integer[]> createPattern();
-
-	/**
-	 * Returns representation of current pattern in Image form
-	 * 
-	 * @param path
-	 *            is the image path
-	 * @return Image of the current pattern
-	 * @throws IOException
-	 *             if there is error with reading an Image
-	 */
-	public BufferedImage createImage(String path) throws IOException;
 }
